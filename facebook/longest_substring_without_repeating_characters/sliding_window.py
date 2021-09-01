236ca7320c0e3fcf985b5f0f2282f16744dd99b7
tykm <tylerkim01@gmail.com>
2021-08-31 23:11:22 -0400

    Finalizing

--- facebook/longest_substring_without_repeating_characters/sliding_window.py
+++ facebook/longest_substring_without_repeating_characters/sliding_window.py
@@ -2,18 +2,27 @@ def sliding_window(s):
     hm = {}
     begin = end = 0
     size = len(s)
-    longest = 1
+    longest = 0
     while end < size:
-        curr_letter = s[end]
-        if hm.get(curr_letter):
+        letter = s[end]
+        if letter in hm:
             longest = max(longest, end - begin)
-            begin = end + 1
-            hm = {}
-        else:
-            hm[curr_letter] = True
-        end += 1 
-    return longest
+            begin = max(hm[letter] + 1, begin)
+        hm[letter] = end
+        end += 1
+    return max(longest, end - begin)
 
-s = "abcabcbb"
-s = 'pwwkew'
+#s = "abcabcbb"
+#s = 'pwwkew'
+s = ' '
 print(sliding_window(s))
+
+'''
+p, p = 0, b=0 e=1
+w, w = 1, b=0, e=2
+w, longest = 3
+
+
+
+
+'''
