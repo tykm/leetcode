818b32c42939e06d7a7819ce5e0e4a0d3899418b
tykm <tylerkim01@gmail.com>
2021-09-02 22:40:18 -0400

    Done

--- facebook/3sum/three_pointers.py
+++ facebook/3sum/three_pointers.py
@@ -1,19 +1,36 @@
+# Source: https://www.youtube.com/watch?v=erEHQO0xljc
+
 def threesum(nums: list[int]) -> list[list[int]]:
     ans = []
     nums.sort()
-    for i in range(len(num) - 2):
-        curr = nums[i]
-        left = i + 1
-        right = len(nums) - 1
-
-        while left < right:
-            
-
-
+    #print(nums)
+    for i in range(len(nums) - 2):
+        # If there are at least 2 nums left and anchor is duplicate with anchor + 1, skip anchor
+        if i > 0 and nums[i] == nums[i - 1]:
+            print(i)
+            continue
+        l = i + 1
+        r = len(nums) - 1
+        while l < r:
+            sm = nums[i] + nums[l] + nums[r]
+            if sm < 0:
+                l += 1
+            elif sm > 0:
+                r -= 1
+            else:   # sm = 0
+                ans.append([nums[i], nums[l], nums[r]])
+                while l < r and nums[l] == nums[l + 1]:
+                    l += 1
+                while l < r and nums[r] == nums[r - 1]:
+                    r -= 1
+                l += 1
+                r -= 1
+    
+    
     return ans
 
 
 nums = [-1,0,1,2,-1,-4]
-#nums = [-1,0,1,2]
+nums = [0,0,0]
 
 print(threesum(nums))
