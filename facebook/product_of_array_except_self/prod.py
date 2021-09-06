2ed360c92617450a6c8bad182aafc5547e3d3b1c
tykm <tylerkim01@gmail.com>
2021-09-06 14:38:04 -0400

    Two passes, one forwards one backwards

--- facebook/product_of_array_except_self/prod.py
+++ facebook/product_of_array_except_self/prod.py
@@ -1,13 +1,16 @@
 def product_except_self(nums: list[int]) -> list[int]:
     n = len(nums)
     ans = [1] * n
-    for i in range(n):
-        print(nums[n-i-1])
-        
-        
+    for i in range(n-1):
+        ans[i+1] = nums[i] * ans[i]
+    for i in range(n-1, 0, -1):
+        ans[i-1] = ans[i-1] * nums[i]
+        nums[i-1] = nums[i-1] * nums[i]
     return ans
 
-nums = [1,2,3,4]
-[ 2*3*4, 1 * 3*4, 1*2 * 4, 1*2*3 ]
+nums =  [1     , 2      , 3      , 4     ]
+     #  [1     , 1      , 1      , 1     ]
+     #  [1     , 1      , 1*2    , 1*2*3 ]
+     #  [ 2*3*4, 1 * 3*4, 1*2 * 4, 1*2*3 ]
 []
 print(product_except_self(nums))
