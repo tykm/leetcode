b919fc4db1a78a5f521c4165046556b9911c984f
tykm <tylerkim01@gmail.com>
2021-11-02 19:37:45 -0400

    Done with Google OA prep

---
+++ google/OA/concatenate_palindromes.py
@@ -0,0 +1,12 @@
+'''
+Given an array of strings, all of which have length 2, determine the max palindrome length that can be made using concatenation.
+
+Example input:
+['ck', 'bd', 'cz', 'kc'] = 4. "ckkc"
+['ck', 'bd', 'aa', 'kc'] = 6. "ckaakc"
+
+Edge cases:
+1. Double letters
+    a. 'oo', 'kk', etc
+        i. Odd/even double letters matter
+'''
---
+++ google/OA/rings.py
@@ -0,0 +1,9 @@
+'''
+There are 10 rods labeled '0'-'9'. You can place rings of colors Red, Green, and Blue on these rods. In order to receive the point, a rod must have all 3 color denominationson on it.
+
+You are given a string consisting of rod number and color pairs. Return the amount of points you receive.
+
+Example strings:
+'R6B9G1B6G6' - 1 point
+'R1B2G2' - 0 points
+'''
