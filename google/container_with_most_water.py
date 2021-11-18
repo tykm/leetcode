435793ffc9eb279d1cc2b5a46e0ddcc2346b2faf
tykm <tylerkim01@gmail.com>
2021-11-18 14:08:18 -0500

    Optimal. Runtime: O(n), Space: O(1)

--- google/container_with_most_water.py
+++ google/container_with_most_water.py
@@ -1,5 +1,3 @@
-# Kinda works. Doesn't work for case [0, 0]
-
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         if len(height) == 0:
@@ -10,9 +8,9 @@ class Solution:
         mx = 0
         
         while left != right:
-            #print(left, right, height[left], height[right])
-            area = (right - left - 1) * max(height[left], height[right])
+            area = (right - left) * min(height[left], height[right])
             mx = max(mx, area)
+            #print(left, right, height[left], height[right], area, mx)
             
             if height[left] > height[right]:
                 right -= 1
