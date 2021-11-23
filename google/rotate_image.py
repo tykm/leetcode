66cceffa8e124a51391277bcd324eb8c2a292fdd
tykm <tylerkim01@gmail.com>
2021-11-23 17:54:57 -0500

    Incorrect implementation but right idea

--- google/rotate_image.py
+++ google/rotate_image.py
@@ -26,3 +26,26 @@ where max = n
 2,1 = 1,1
 '''
 
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix) - 1
+        
+        # Function to return new coordinates
+        def rot(x, y):
+            return y, n-x
+        
+        for col in range((n+1)//2):
+            for row in range(col, n):
+                first_col, first_row = rot(col, row)
+                second_col, second_row = rot(first_col, first_row)
+                third_col, third_row = rot(second_col, second_row)
+                #print(col, row, first_col, first_row, second_col, second_row, third_col, third_row)
+                curr = matrix[col][row]
+                first = matrix[first_col][first_row]
+                second = matrix[second_col][second_row]
+                third = matrix[third_col][third_row]
+                #print(curr, first, second, third)
+                matrix[col][row] = third
+                matrix[first_col][first_row] = curr
+                matrix[second_col][second_row] = first
+                matrix[third_col][third_row] = second
