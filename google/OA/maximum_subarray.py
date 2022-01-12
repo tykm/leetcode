dbf5c992a429a586b6e6227a088b1d0f1f111163
tykm <tylerkim01@gmail.com>
2022-01-12 16:44:09 -0500

    Cleaning up the negative answer case

--- google/OA/maximum_subarray.py
+++ google/OA/maximum_subarray.py
@@ -3,11 +3,8 @@ class Solution:
         mx = float(-inf)
         curr = 0
         for num in nums:
-            # Take care of answer < 0 case
-            if num > mx:
-                mx = num
-            if curr < 0:
-                curr = 0
             curr += num
             mx = max(mx, curr)
+            if curr < 0:
+                curr = 0
         return mx
