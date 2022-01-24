1f8c108cf9cee631e6e785189e810f07dcf830e6
tykm <tylerkim01@gmail.com>
2022-01-24 15:09:00 -0500

    Single pass O(n * k)

--- blind75/not_week1/group_anagrams.py
+++ blind75/not_week1/group_anagrams.py
@@ -1,13 +1,22 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # { ['a','e','t']:["eat", "tea"], ...}
-        anagrams = {}
+        # letters stores a count of each letter
+        letters = [0] * 26
+        # words stores the counts of anagrams
+        # { (0, 1, 0, 2...): ["eat", "tea"] }
+        words = {}
+        
         
         for word in strs:
-            sort_word = tuple(sorted(word))
-            if sort_word in anagrams:
-                anagrams[sort_word].append(word)
+            letters = [0] * 26
+            # Iterate through word and store letter count
+            for letter in word:
+                letter_value = ord(letter) - ord('a')
+                letters[letter_value] += 1
+            letters = tuple(letters)
+            if letters not in words:
+                words[letters] = [word]
             else:
-                anagrams[sort_word] = [word]
-        
-        return anagrams.values()
+                words[letters].append(word)
+                
+        return words.values()
