1c79e58f4bba71b2747f3f945be157f7a66e3f0a
tykm <tylerkim01@gmail.com>
2022-01-26 22:55:16 -0500

    My rendition with pseudocode explanation

--- blind75/not_week1/shortest_path_to_get_food.py
+++ blind75/not_week1/shortest_path_to_get_food.py
@@ -1,29 +1,38 @@
 class Solution:
     def getFood(self, grid: List[List[str]]) -> int:
-        q = collections.deque()
         rows = len(grid)
         cols = len(grid[0])
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        q = collections.deque()
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
         
-        # Find starting position
+        # Find the start location and add it to queue and mark as visited
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == "*":
+                    # (row, col, steps)
+                    grid[i][j] = "/"
                     q.append((i, j, 0))
-                    break
         
+        # BFS through queue
         while q:
             row, col, steps = q.popleft()
+            
+            # Go in every direction
             for y, x in directions:
-                new_row = row + y
-                new_col = col + x
-                # If new tile is valid
-                if 0 <= new_row < rows and 0 <= new_col < cols and grid[new_row][new_col] in {"#","O"}:
-                    # If new tile is food
-                    if grid[new_row][new_col] == "#":
-                        return steps + 1
-                    # Log new tile as visited
-                    grid[new_row][new_col] = "/"
-                    # Add new tile to bfs queue
-                    q.append((new_row, new_col, steps + 1))
+                # newrow, newcol
+                nr = row + y
+                nc = col + x
+                # If tile is OOB or Obstacle or Visited, continue
+                if (nr < 0 or nr >= rows or nc < 0 or nc >= cols):
+                    continue
+                elif grid[nr][nc] in {"X", "/"}:
+                    continue
+                # Else    
+                # Check for Food
+                if grid[nr][nc] == "#":
+                    return steps + 1
+                
+                # Mark as visited and add to queue
+                grid[nr][nc] = "/"
+                q.append((nr, nc, steps + 1))
         return -1
